8cfebe5da9c854db4df84e660f1b5cc63971b692
laxmimerit <laxmimerit@gmail.com>
2025-11-18 13:10:28 +0530

    added airbnb mcp server

--- 13. LangGraph MCP/airbnb_mcp.py
+++ 13. LangGraph MCP/airbnb_mcp.py
@@ -1,18 +1,12 @@
 """Simple Airbnb MCP module."""
+
 from langchain_ollama import ChatOllama
 from langgraph.graph import StateGraph, START, END
 from langgraph.prebuilt import ToolNode, tools_condition
 from langchain_core.messages import HumanMessage
 
-from langchain_mcp_adapters.client import MultiServerMCPClient
-import asyncio
-
 from typing_extensions import TypedDict, Annotated
 import operator
-
-# MCP GITHUB
-# https://github.com/laxmimerit/MCP-Mastery-with-Claude-and-Langchain
-# https://github.com/langchain-ai/langchain-mcp-adapters
 
 # Config
 LLM_MODEL = "qwen3"
@@ -20,61 +14,81 @@ BASE_URL = "http://localhost:11434"
 
 llm = ChatOllama(model=LLM_MODEL, base_url=BASE_URL, temperature=0)
 
+
 class AgentState(TypedDict):
     messages: Annotated[list, operator.add]
 
+
+######## MCP SETUP ###############
+# MCP GITHUB
+# https://github.com/laxmimerit/MCP-Mastery-with-Claude-and-Langchain
+# https://github.com/langchain-ai/langchain-mcp-adapters
+
+from langchain_mcp_adapters.client import MultiServerMCPClient
+import asyncio
+
+
 async def get_tools():
+
     client = MultiServerMCPClient(
         {
             "airbnb": {
                 "command": "npx",
-                "args": [
-                    "-y",
-                    "@openbnb/mcp-server-airbnb",
-                    "--ignore-robots-txt"
-                ],
+                "args": ["-y", "@openbnb/mcp-server-airbnb", "--ignore-robots-txt"],
                 "transport": "stdio"
             }
         }
     )
+
+
     tools = await client.get_tools()
-    print(f"Loaded {len(tools)}")
-    print(f"Tools: {tools}")
+
+    print(f"Loaded {len(tools)} Tools")
+    print(f"Tools Available: {tools}")
 
     return tools
 
-# create in-function agent node
-async def agent(state: AgentState):
+
+async def agent_node(state: AgentState):
     tools = await get_tools()
+
     llm_with_tools = llm.bind_tools(tools)
+
     response = llm_with_tools.invoke(state['messages'])
+
     return {'messages': [response]}
 
-# make sure to install npx and node
-async def create_agent():
 
+async def create_agent():
     tools = await get_tools()
 
     builder = StateGraph(AgentState)
-    builder.add_node('agent', agent)
+    builder.add_node('agent', agent_node)
     builder.add_node('tools', ToolNode(tools))
+
 
     # add edges
     builder.add_edge(START, 'agent')
     builder.add_edge('tools', 'agent')
+
     builder.add_conditional_edges('agent', tools_condition)
 
     graph = builder.compile()
+
     return graph
+
 
 async def search(query):
     agent = await create_agent()
     result = await agent.ainvoke({'messages': [HumanMessage(query)]})
+
     response = result['messages'][-1].content
-    print(f"\n{response}\n")
+
+    print(response)
 
     return response
 
+
 if __name__=="__main__":
-    # asyncio.run(create_agent())
-    asyncio.run(search("Find premium hotels in Mumbai"))
+    query = "Show me premium hotels for party in Mumbai"
+    asyncio.run(search(query))
